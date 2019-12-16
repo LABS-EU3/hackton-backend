f29c350f24fe92e330ec362e6ef48b9c8257703c
PascalUlor <pascalulor@yahoo.com>
2019-12-16 18:53:43 +0100

    add linter configs: add prettier config edit eslint rc config install prettier [fixes linting]

--- api/server.js
+++ api/server.js
@@ -1,7 +1,8 @@
-const express = require("express");
-const helmet = require("helmet");
-const cors = require("cors");
-const dotenv = require("dotenv");
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
 dotenv.config();
 
 const server = express();
@@ -10,19 +11,18 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
+server.get('/', (req, res) => {
+  res.status(200).json({
+    status: 200,
+    message: 'Hello from Hackton backend!'
+  });
+});
 
-server.get("/",(req,res) => {
-    res.status(200).json({
-        status: 200,
-        message:"Hello from Hackton backend!"
-    });
-})
-
-server.get("*",(req,res) => {
-    res.status(404).json({
-        status: 404,
-        message:"Invalid route!"
-    });
-})
+server.get('*', (req, res) => {
+  res.status(404).json({
+    status: 404,
+    message: 'Invalid route!'
+  });
+});
 
 module.exports = server;
