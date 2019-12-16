10737a1edb2cc6fdd9278a4eda0ea6af2686c487
PascalUlor <pascalulor@yahoo.com>
2019-12-16 18:22:51 +0100

    fix errors in server test: add done to default route return fix travis build issues [fixes bug in server test file]

--- api/server.spec.js
+++ api/server.spec.js
@@ -2,18 +2,17 @@ const request = require('supertest');
 const server = require('./server');
 
 describe('server', () => {
-    it('[GET] / WORKS!', () => {
-        return request(server)
-          .get('/')
-          .expect(200)
-          .expect('Content-Type', /json/)
-          .then(res => {
-            expect(res.body).toEqual({ status: 200, message: 'Hello from Hackton backend!' });
-          });
+    it('[GET] / WORKS!', async () => {
+        const res = await request(server)
+        .get('/')
+        .expect(200)
+        .expect('Content-Type', /json/);
+      expect(res.body).toEqual({ status: 200, message: 'Hello from Hackton backend!' });
       });
-      it('[GET] / Fails!', () => {
+      it('[GET] / Fails!', (done) => {
         return request(server)
           .get('/wrong')
-          .expect(404);
+          .expect(404)
+          .end(done);
       });
 })
