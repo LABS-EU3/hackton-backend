c925a186e3e3459d6641f84aceb113e83fdd94f1
Mildred Awuor <otieno.awuor.m@gmail.com>
2019-12-17 14:35:09 +0300

    setup db:
    created database
    [finishes setup db]

--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,49 @@
-// Update with your config settings.
-
+// // Update with your config settings.
 module.exports = {
-
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
+    client: 'pg',
+    connection:'postgres://localhost:3430/hackton_db',
+    migrations: {
+      directory: 'data/migrations'
+    },
+    seeds: {
+      directory: 'data/seeds'
+    },
+    useNullAsDefault: true
   },
 
+  test: {
+    client: 'pg',
+    connection:'postgres://localhost:3430/hackton_test_db',
+    migrations: {
+      directory: 'data/migrations'
+    },
+    seeds: {
+      directory: 'data/seeds'
+    },
+    useNullAsDefault: true
+  },
   staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      directory: 'data/migrations'
     },
-    pool: {
-      min: 2,
-      max: 10
+    seeds: {
+      directory: 'data/seeds'
     },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+    useNullAsDefault: true
   }
 
-};
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: 'data/migrations'
+    },
+    seeds: {
+      directory: 'data/seeds'
+    },
+    useNullAsDefault: true
+  }
+}
