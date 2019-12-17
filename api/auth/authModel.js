f7197b71dd7a878b5afc24f64591385fbd9985a2
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-12-17 20:06:39 +0000

    create model to add user credentials: [finishes sign up endpoint]

--- api/auth/authModel.js
+++ api/auth/authModel.js
@@ -1,2 +1,19 @@
 const db = require('../../data/dbConfig');
 
+function getUserId(id) {
+    return db('users')
+    .where('users.id', id)
+    .select('fullname', 'username', 'email', 'bio')
+    .first()
+}
+
+function addUser(user) {
+    return db('users')
+    .insert(user, 'user.id')
+    .then(([id]) => this.getUserId(id))
+}
+
+module.exports = {
+    getUserId,
+    addUser
+}
