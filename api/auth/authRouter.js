5cfa95bd6bede5969539133299563c84d1fdbebb
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-12-18 06:12:16 +0000

    create endpoint for login: [finishes login endpoint]

--- api/auth/authRouter.js
+++ api/auth/authRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const db = require('./authModel');
+const generateToken = require('../../utils/generateToken')
 const router = express.Router()
 
 const bodyValidator = require('../../utils/validator')
@@ -17,6 +18,29 @@ router.post('/register', bodyValidator, (req, res) => {
     .catch(error => {
         res.status(500).json({
             message: 'Couldnt register user: ' + error.message, data: error
+        })
+    })
+})
+
+router.post('/login', (req, res) => {
+    let {username, password} = req.body;
+
+    db.getUserBy({username})
+    .then( user => {
+        if(user && bcrypt.compareSync(password, user.password)) {
+            const token = generateToken(user)
+            res.status(200).json({
+                token: token
+            })
+        } else {
+            res.status(400).json({
+                message: 'Invalid password!'
+            })
+        }
+    })
+    .catch(error => {
+        res.status(500).json({
+            message: 'Internal server error' + error.message
         })
     })
 })
