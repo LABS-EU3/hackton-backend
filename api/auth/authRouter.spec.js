aeb95bd06ab75ea2e29bd175fb75d8713b1888ea
PascalUlor <pascalulor@yahoo.com>
2019-12-18 18:16:27 +0100

    rewrite authrouter befroeeach function

--- api/auth/authRouter.spec.js
+++ api/auth/authRouter.spec.js
@@ -5,72 +5,72 @@ const db = require('../../data/dbConfig');
 let token;
 
 const addUser = {
-  username: "test",
-  password: "test",
-  bio: "I am a robot",
-  email: "test@email.com",
-  fullname: "tests tests"
+  username: 'test',
+  password: 'test',
+  bio: 'I am a robot',
+  email: 'test@email.com',
+  fullname: 'tests tests'
 };
 
 beforeEach(async () => {
-    await db("event_categories").del();
-    await db("events").del();
-    await db("users").del();
+  await db.raw('TRUNCATE TABLE event_categories,events, users CASCADE');
+  // await db("event_categories").del();
+  // await db("events").del();
+  // await db("users").del();
 
-//   const a = await request(server)
-//     .post("/api/auth/register")
-//     .send(addUser);
+  //   const a = await request(server)
+  //     .post("/api/auth/register")
+  //     .send(addUser);
 
-//   token = res.body.token;
+  //   token = res.body.token;
   //   console.log(token);
 });
 
-describe("api/auth/* endpoints", () => {
-  describe("[POST] /api/auth", () => {
-    test("should return 201 Created", async () => {
+describe('api/auth/* endpoints', () => {
+  describe('[POST] /api/auth', () => {
+    test('should return 201 Created', async () => {
       const response = await request(server)
-        .post("/api/auth/register")
+        .post('/api/auth/register')
         .send(addUser);
       expect(response.status).toBe(201);
     });
 
     test('should return user credentials', async () => {
-        const response = await request(server)
-            .post('/api/auth/register')
-            .send(addUser);
-            expect(response.body.user.email).toBe(addUser.email)
-            expect(response.body.user.bio).toBe(addUser.bio)
-            expect(response.body.user.username).toBe(addUser.username)
-
-    })
+      const response = await request(server)
+        .post('/api/auth/register')
+        .send(addUser);
+      expect(response.body.user.email).toBe(addUser.email);
+      expect(response.body.user.bio).toBe(addUser.bio);
+      expect(response.body.user.username).toBe(addUser.username);
+    });
 
     test('Email is required', async () => {
-        const userCopy = {...addUser}
-        delete userCopy.email
+      const userCopy = { ...addUser };
+      delete userCopy.email;
 
-        const response = await request(server)
+      const response = await request(server)
         .post('/api/auth/register')
-        .send(userCopy)
+        .send(userCopy);
 
-        expect(response.status).toBe(400)
-    })
+      expect(response.status).toBe(400);
+    });
 
     test('Password is required', async () => {
-        const userCopy = {...addUser}
-        delete userCopy.password
+      const userCopy = { ...addUser };
+      delete userCopy.password;
 
-        const response = await request(server)
+      const response = await request(server)
         .post('/api/auth/register')
-        .send(userCopy)
+        .send(userCopy);
 
-        expect(response.status).toBe(400)
-    })
+      expect(response.status).toBe(400);
+    });
 
-    test("should return a token", async () => {
-        const response = await request(server)
-          .post("/api/auth/register")
-          .send(addUser);
-        expect(response.body.token).not.toBe(undefined);
+    test('should return a token', async () => {
+      const response = await request(server)
+        .post('/api/auth/register')
+        .send(addUser);
+      expect(response.body.token).not.toBe(undefined);
     });
   });
 });
