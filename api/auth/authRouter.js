7bb0de1c8a0a22645b6c48974a2b47caf5c2d1c9
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-12-18 09:25:34 +0000

    use id and email to generate token: [finishes signup endpoint]

--- api/auth/authRouter.js
+++ api/auth/authRouter.js
@@ -3,7 +3,6 @@ const bcrypt = require('bcrypt');
 const db = require('./authModel');
 const generateToken = require('../../utils/generateToken')
 const router = express.Router()
-
 const bodyValidator = require('../../utils/validator')
 
 router.post('/register', bodyValidator, (req, res) => { //endpoint to register
@@ -22,29 +21,6 @@ router.post('/register', bodyValidator, (req, res) => { //endpoint to register
     .catch(error => {
         res.status(500).json({
             message: 'Couldnt register user: ' + error.message, data: error
-        })
-    })
-})
-
-router.post('/login', bodyValidator, (req, res) => {
-    let {username, password} = req.body;
-
-    db.getUserBy({username})
-    .then( user => {
-        if(user && bcrypt.compareSync(password, user.password)) {
-            const token = generateToken(user)
-            res.status(200).json({
-                token: token
-            })
-        } else {
-            res.status(400).json({
-                message: 'Invalid password!'
-            })
-        }
-    })
-    .catch(error => {
-        res.status(500).json({
-            message: 'Internal server error' + error.message
         })
     })
 })
