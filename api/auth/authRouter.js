c3778ba2844b78cf4203c88f68bf3193f349baae
PascalUlor <pascalulor@yahoo.com>
2019-12-22 03:31:28 +0100

    implement auth0 login with github and google

--- api/auth/authRouter.js
+++ api/auth/authRouter.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const GitHubStrategy = require('passport-github2').Strategy;
 const db = require('./authModel');
 const generateToken = require('../../utils/generateToken');
 const bodyValidator = require('../../utils/validator');
@@ -56,6 +57,7 @@ router.post('/login', bodyValidator, (req, res) => {
     });
 });
 
+// google
 router.use(passport.initialize());
 
 passport.serializeUser((user, done) => {
@@ -75,15 +77,15 @@ passport.use(
     },
     async function(accessToken, refreshToken, profile, done) {
       const userCredentials = {
-        id: profile.id,
         username: profile.name.givenName,
-        password: profile.id,
+        password: bcrypt.hashSync('Hackton', 15),
         email: profile.emails[0].value,
         fullname: profile.displayName
       };
-
       const user = await db.createOrFindUser(userCredentials);
+      const token = await generateToken(user);
       server.locals = user;
+      server.locals.token = token;
       done(null, {
         accessToken,
         refreshToken,
@@ -102,11 +104,72 @@ router.get(
   '/google/callback',
   passport.authenticate('google', { failureRedirect: '/login' }),
   (req, res) => {
-    server.locals.profile = req.user.profile;
-    const token = generateToken(req.user.profile);
+    res.locals.token = server.locals.token;
     res.redirect(
-      `https://hackton-frontend-g3tpfw81r.now.sh/register?google=${token}`
+      `${process.env.REDIRECT_URL_GOOGLE}/register?google=${server.locals.token}`
     ); // redirect with the token so that the frontend can extract it for user details
+  }
+);
+
+// github
+
+router.use(passport.initialize());
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.use(
+  new GitHubStrategy(
+    {
+      clientID: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      callbackURL: process.env.URL || 'http://localhost:4000/api/auth/github'
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      const { id, username, _json } = profile;
+
+      const userCredentials = {
+        id,
+        fullname: _json.name,
+        username,
+        email: _json.email || id,
+        password: bcrypt.hashSync('Hackton', 15)
+      };
+      const user = await db.createOrFindUser(userCredentials);
+      const token = generateToken(user);
+      server.locals = user;
+      server.locals.token = token;
+
+      done(null, {
+        accessToken,
+        refreshToken,
+        profile
+      });
+    }
+  )
+);
+
+router.get(
+  '/github',
+  passport.authenticate('github', {
+    scope: ['profile', 'email']
+  }),
+  async (req, res) => {
+    try {
+      const data = server.locals;
+      if (!data) {
+        res.status(400).json({
+          ErrorMessage: 'Github Authentication Failed'
+        });
+      }
+      res.locals.token = server.locals.token;
+      res.redirect(process.env.REDIRECT_URL);
+    } catch (error) {
+      return error;
+    }
   }
 );
 
