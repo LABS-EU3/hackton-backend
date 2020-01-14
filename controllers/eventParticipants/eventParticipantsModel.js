f702790ea8dc82b389544e2308a3848bd69762f8
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-01-14 20:04:50 +0000

    create model to add participant credentials

--- controllers/eventParticipants/eventParticipantsModel.js
+++ controllers/eventParticipants/eventParticipantsModel.js
@@ -1,18 +1,27 @@
 const db = require('../../data/dbConfig');
 
-function getById () {
+async function getById (id) {
     const eventSelected = await db('event_participants')
     .where('event_participants.id', id)
     .first()
     return eventSelected;
 }
 
-function getAll () {
+async function getAll () {
     const allSelectedEvents = await db('event_participants')
     return allSelectedEvents;
 }
 
+async function addCredentials (credentials) {
+    const newCredentials = await db('event_participants')
+    .insert(credentials)
+    .returning('*')
+    .then(data => data[0])
+    return newCredentials;
+}
+
 module.exports = {
     getById,
-    getAll
+    getAll,
+    addCredentials
 }
