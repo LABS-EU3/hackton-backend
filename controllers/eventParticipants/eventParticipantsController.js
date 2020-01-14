b4ee4eb7eb3658998ce8f56d4fcadf000bdf1f9b
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-01-14 21:33:09 +0000

    write function to get all events

--- controllers/eventParticipants/eventParticipantsController.js
+++ controllers/eventParticipants/eventParticipantsController.js
@@ -11,3 +11,13 @@ function handleEventGetById (res, req) {
             return requestHandler.error(res, 404, `Event Not Found ${error.message}`)
         })
 }
+
+function handleEventGetAll (res, req) {
+    db.getAll()
+    .then(data => {
+        return requestHandler.success(res, 200, 'All events retrieved successfully', data );
+    })
+    .catch(error => {
+        return requestHandler.error(res, 404, `Events Not Found ${error.message}`)
+    })
+}
