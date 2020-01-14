3c96b514700a474d2c101620da55d371735e43b6
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-01-14 18:35:00 +0000

    update database and run migrations

--- data/migrations/20200114172910_04-event_participants.js
+++ data/migrations/20200114172910_04-event_participants.js
@@ -1,8 +1,24 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('event_participants', table => {
+    table.increments();
+
+    table
+      .integer('event_id')
+      .references('id')
+      .inTable('events')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
+
+    table
+      .integer('user_id')
+      .references('id')
+      .inTable('users')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
+
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists('event_participants');
 };
