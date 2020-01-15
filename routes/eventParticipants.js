286154877898d4876a7d34524d5994485353a2fb
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-01-15 10:29:51 +0000

    create endpoint to get all users registered event

--- routes/eventParticipants.js
+++ routes/eventParticipants.js
@@ -1,5 +1,13 @@
 const { Router } = require('express');
 const authenticate = require('../api/auth/authenticate');
 const {
-    
-}
+    handleEventGetById,
+    handleEventGetAll,
+    handleEventRegistration,
+    handleEventDelete
+} = require('../controllers/eventParticipants/eventParticipantsController');
+
+const EventParticipantValidator = require('../middlewares/EventParticipantsValidator');
+const router = Router();
+
+router.get('/', authenticate, handleEventGetAll);
