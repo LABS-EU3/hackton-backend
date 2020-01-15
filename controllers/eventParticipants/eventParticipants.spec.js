2d83e5b2b0f9673b772810053b2c244ce524e1b0
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-01-15 14:46:17 +0000

    create a dummy user

--- controllers/eventParticipants/eventParticipants.spec.js
+++ controllers/eventParticipants/eventParticipants.spec.js
@@ -2,3 +2,9 @@ const request = require('supertest');
 const server = require('../../api/server');
 const db = require('../../data/dbConfig');
 
+let token;
+
+const addUser = {
+    email: 'test@mail.com',
+    password: 'test'
+}
