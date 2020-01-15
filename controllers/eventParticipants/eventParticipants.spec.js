a94cabad01e14614aba1b88f48d55d7f37207b7a
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-01-15 15:13:59 +0000

    create before each

--- controllers/eventParticipants/eventParticipants.spec.js
+++ controllers/eventParticipants/eventParticipants.spec.js
@@ -5,6 +5,14 @@ const db = require('../../data/dbConfig');
 let token;
 
 const addUser = {
-    email: 'test@mail.com',
-    password: 'test'
-}
+  email: 'test@mail.com',
+  password: 'test'
+};
+
+beforeEach(async () => {
+  await db.raw('TRUNCATE TABLE event_categories, users, events CASCADE');
+  const response = await request(server)
+    .post('/api/auth/register')
+    .send(addUser);
+});
+
