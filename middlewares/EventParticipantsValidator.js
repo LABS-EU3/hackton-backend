18430386b666f1827a481fb476abc6b9a08bd9df
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-01-15 08:15:25 +0000

    create validator file and import required files

---
+++ middlewares/EventParticipantsValidator.js
@@ -0,0 +1,6 @@
+/* eslint-disable camelcase */
+const checkItem = require('../utils/checkInputs');
+const requestHandler = require('../utils/requestHandler');
+const eventParticipantsModel = require('../controllers/eventParticipants/eventParticipantsModel');
+require('dotenv').config();
+
