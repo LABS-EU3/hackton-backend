88e0d0380f856ae61a76c15f3673e0cd43d6a4c2
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-01-15 08:23:52 +0000

    create a function to validate id

--- middlewares/EventParticipantsValidator.js
+++ middlewares/EventParticipantsValidator.js
@@ -4,3 +4,47 @@ const requestHandler = require('../utils/requestHandler');
 const eventParticipantsModel = require('../controllers/eventParticipants/eventParticipantsModel');
 require('dotenv').config();
 
+/**
+ * Validates all routes
+ * @class EventParticipantsValidator
+ */
+module.exports = class EventValidator {
+    /**
+     * Validates all event details
+     * @param {obj} req
+     * @param {obj} res
+     * @param {obj} next
+     * @returns {obj} Validation error messages or contents of req.body
+     */
+    static async validateID(req, res, next) {
+      // validates provided ID is a number
+      const { id } = req.params;
+      const check = checkItem({ id });
+  
+      if (Object.keys(check).length > 0) {
+        return res.status(400).json({
+          statusCode: 400,
+          data: [check]
+        });
+      }
+      eventParticipantsModel
+        .getById(id)
+        .then(data => {
+          if (data.length === 0) {
+            return requestHandler.error(
+              res,
+              404,
+              'This event id cannot be found,please provide a valid id'
+            );
+          }
+          req.event = data;
+          return next();
+        })
+        .catch(error => {
+          return requestHandler.error(res, 500, `Server error ${error}`);
+        });
+    }
+  
+    
+  };
+  
