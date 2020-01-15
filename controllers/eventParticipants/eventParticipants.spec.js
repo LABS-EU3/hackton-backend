a49bd50aed618055dcc863ab4236a5c8f278e860
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-01-15 20:09:04 +0000

    create dummy event

--- controllers/eventParticipants/eventParticipants.spec.js
+++ controllers/eventParticipants/eventParticipants.spec.js
@@ -9,6 +9,17 @@ const addUser = {
   password: 'test'
 };
 
+const newEvent = {
+  event_title: "Test Hackathon", 
+ 	start_date: "01-02-2022", 
+ 	end_date: "02-02-2022", 
+  event_description: "A test for backend It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of  (The Extremes of Good and Evil) by Cicero, written in 45 BC. Thpk;lis book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum",
+  location: "Remotes", 
+ 	guidelines: "Run yarn or npm test. Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 200lj0 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 ofhe Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum", 
+ 	participation_type: "team", 
+ 	category_id: 2 
+}
+
 beforeEach(async () => {
   await db.raw('TRUNCATE TABLE event_categories, users, events CASCADE');
   const response = await request(server)
@@ -16,3 +27,14 @@ beforeEach(async () => {
     .send(addUser);
 });
 
+describe('Event participants endpoints', () => {
+  test('user can register for as a participant for an event', () => {
+    const response = await request(server)
+    .post('/api/auth/login')
+    .send(addUser);
+    token = response.body.body.token;
+    const eventCreation = await request(server)
+    .post('/api/events')
+    .send()
+  })
+})
