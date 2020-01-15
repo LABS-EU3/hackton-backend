c71e97c763ea5eff7f3d6c80f7e5434563429a0a
Mildred Awuor <otieno.awuor.m@gmail.com>
2020-01-15 18:26:16 +0300

    removed tags controllers

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,13 @@ const { Router } = require('express');
 const authRoutes = require('./auth');
 const eventRoutes = require('./events');
 const categoriesRoutes = require('./categories.js');
-const tagsRoutes = require('./tags');
+
 
 const router = Router();
 
 router.use('/auth', authRoutes);
 router.use('/events', eventRoutes);
 router.use('/event-category', categoriesRoutes);
-router.use('/event-tags', tagsRoutes);
+
 
 module.exports = router;
