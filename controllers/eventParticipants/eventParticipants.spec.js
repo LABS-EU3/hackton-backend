bd03d19143d3f13e3e5ce478507d21d54f633989
PascalUlor <pascalulor@yahoo.com>
2020-01-16 16:05:24 +0100

    fix date format

--- controllers/eventParticipants/eventParticipants.spec.js
+++ controllers/eventParticipants/eventParticipants.spec.js
@@ -11,8 +11,8 @@ const addUser = {
 
 const newEvent = {
   event_title: 'Test Hackathon',
-  start_date: '01-02-2022',
-  end_date: '02-02-2022',
+  start_date: '2019-12-23',
+  end_date: '2019-12-23',
   event_description:
     'A test for backend It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of  (The Extremes of Good and Evil) by Cicero, written in 45 BC. Thpk;lis book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum',
   location: 'Remotes',
