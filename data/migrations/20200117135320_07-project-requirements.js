a3f3069d7a0947700d20a576d1829ebb3cd1d7d6
Mildred Awuor <otieno.awuor.m@gmail.com>
2020-01-17 14:03:29 +0300

    created project requirements migration file

---
+++ data/migrations/20200117135320_07-project-requirements.js
@@ -0,0 +1,24 @@
+/* eslint-disable func-names */
+exports.up = function(knex) {
+  return knex.schema.createTable('project_requirements', table => {
+    table.increments();
+    table
+      .integer('event_id')
+      .unsigned()
+      .notNullable();
+    table
+      .foreign('event_id')
+      .references('events.id')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
+    table.boolean('video_url');
+    table.boolean('git_url');
+    table.boolean('images');
+    table.boolean('project_writeup');
+    table.timestamps(true, true);
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('project_requirements');
+};
