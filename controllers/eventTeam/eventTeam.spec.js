c390b17dab94ede10bf14abe7cc430c254b997b7
PascalUlor <pascalulor@yahoo.com>
2020-01-17 14:29:02 +0100

    create mock event data

--- controllers/eventTeam/eventTeam.spec.js
+++ controllers/eventTeam/eventTeam.spec.js
@@ -1,42 +1,30 @@
 const request = require('supertest');
 const server = require('../../api/server');
 const db = require('../../data/dbConfig');
+const mockUsers = require('../../data/mock/auth.mock');
 
 let token;
-
-const addUser = {
-  email: 'test1@email.com',
-  password: 'test1234'
-};
-const secondUser = {
-  email: 'test2@email.com',
-  password: 'test2345'
-};
-const thirdUser = {
-  email: 'test3@email.com',
-  password: 'test3456'
-};
 
 beforeEach(async () => {
   await db.raw('TRUNCATE TABLE event_categories,users, events CASCADE');
   const response1 = await request(server) // creates new user before each test
     .post('/api/auth/register')
-    .send(addUser);
+    .send(mockUsers.validInput1);
   const response2 = await request(server) // creates new user before each test
     .post('/api/auth/register')
-    .send(secondUser);
+    .send(mockUsers.validInput2);
   const response3 = await request(server) // creates new user before each test
     .post('/api/auth/register')
-    .send(thirdUser);
+    .send(mockUsers.validInput3);
+  const response = await request(server)
+    .post('/api/auth/login')
+    .send(mockUsers.validInput1);
+  token = response.body.body.token;
 });
 
 describe('user can add/get team members to an event', () => {
   test.only('user can [GET] api/users', async () => {
     // logged in user can successfully fetch a user `
-    const response = await request(server)
-      .post('/api/auth/login')
-      .send(addUser);
-    token = response.body.body.token;
     const response4 = await request(server)
       .get('/api/users')
       .set('Authorization', token);
