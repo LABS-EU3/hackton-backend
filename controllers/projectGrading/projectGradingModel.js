1a250e52eb8e319c8135ec70dca7050a614e0321
Mildred Awuor <otieno.awuor.m@gmail.com>
2020-01-22 19:04:39 +0300

    added judges name to the grading get query, changed event_id to project_event_id in grading table

--- controllers/projectGrading/projectGradingModel.js
+++ controllers/projectGrading/projectGradingModel.js
@@ -8,21 +8,13 @@ async function addGrading(grade) {
   return submittedGrading;
 }
 async function findAllGradingsByEventId(id) {
-  const foundAllGrades = await db('project_grading')
-    .where({
-      event_id: id
-    })
-    .returning('*');
-  return foundAllGrades;
-}
-async function findGrading(id) {
-  const foundSubmission = await db('project_grading as g')
+  const foundAllGrades = await db('project_grading as g')
     .join('project_entries as p', 'p.id', 'g.project_id')
-    .join('events as e', 'e.id', 'g.event_id')
+    .join('events as e', 'e.id', 'g.project_event_id')
     .join('users as u', 'u.id', 'g.judge_id')
     .select(
       'g.project_id',
-      'g.event_id',
+      'g.project_event_id',
       'g.judge_id',
       'g.product_design',
       'g.functionality',
@@ -33,7 +25,39 @@ async function findGrading(id) {
       'g.average_rating',
       'p.project_writeups',
       'p.video_url',
-      'p.git_url'
+      'p.git_url',
+      'p.participant_or_team_name',
+      'u.email as judge_email',
+      'u.fullname as judge_fullname',
+      'g.judge_comments'
+    )
+    .where({ project_event_id: id });
+  return foundAllGrades;
+}
+
+async function findGrading(id) {
+  const foundSubmission = await db('project_grading as g')
+    .join('project_entries as p', 'p.id', 'g.project_id')
+    .join('events as e', 'e.id', 'g.project_event_id')
+    .join('users as u', 'u.id', 'g.judge_id')
+    .select(
+      'g.project_id',
+      'g.project_event_id',
+      'g.judge_id',
+      'g.product_design',
+      'g.functionality',
+      'g.innovation',
+      'g.product_fit',
+      'g.extensibility',
+      'g.presentation',
+      'g.average_rating',
+      'p.project_writeups',
+      'p.video_url',
+      'p.git_url',
+      'p.participant_or_team_name',
+      'u.email as judge_email',
+      'u.fullname as judge_fullname',
+      'g.judge_comments'
     )
     .where({ project_id: id });
   return foundSubmission;
