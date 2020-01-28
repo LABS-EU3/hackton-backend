64e44af7deeee8189d6bedefbb51c4a09817b866
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-01-28 13:38:55 +0100

    import nodemailer

---
+++ utils/emailHandler.js
@@ -0,0 +1,29 @@
+const nodemailer = require('nodemailer');
+
+const email = process.env.EMAIL_ADDRESS
+const password = process.env.PASSWORD
+
+// exports.sendEmail = (subject, recipient, body) => {
+//     const mailOptions = {
+//         from: `Hackton <hackton@portal>`,
+//         to: recipient,
+//         subject: subject,
+//         html: body,
+//         text: 'I hope this message gets through!',
+//     }
+    
+//     const transporter = nodemailer.createTransport({
+//         service: 'gmail',
+//         auth: {
+//             user: email,
+//             pass: password,
+//         }
+//     });
+
+//     console.log(email, password)
+
+//     transporter.sendMail(mailOptions)
+// };
+
+
+  
