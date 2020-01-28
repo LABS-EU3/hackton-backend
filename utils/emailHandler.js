32027dc5b7c0cb27013a36ebaf2fefebc08c1653
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-01-28 13:41:00 +0100

    create mail options

--- utils/emailHandler.js
+++ utils/emailHandler.js
@@ -3,27 +3,25 @@ const nodemailer = require('nodemailer');
 const email = process.env.EMAIL_ADDRESS
 const password = process.env.PASSWORD
 
-// exports.sendEmail = (subject, recipient, body) => {
-//     const mailOptions = {
-//         from: `Hackton <hackton@portal>`,
-//         to: recipient,
-//         subject: subject,
-//         html: body,
-//         text: 'I hope this message gets through!',
-//     }
+exports.sendEmail = (subject, recipient, body) => {
+    const mailOptions = {
+        from: `Hackton <hackton@portal>`,
+        to: recipient,
+        subject: subject,
+        html: body,
+        text: 'I hope this message gets through!',
+    }
     
-//     const transporter = nodemailer.createTransport({
-//         service: 'gmail',
-//         auth: {
-//             user: email,
-//             pass: password,
-//         }
-//     });
+    // const transporter = nodemailer.createTransport({
+    //     service: 'gmail',
+    //     auth: {
+    //         user: email,
+    //         pass: password,
+    //     }
+    // });
 
-//     console.log(email, password)
-
-//     transporter.sendMail(mailOptions)
-// };
+    transporter.sendMail(mailOptions)
+};
 
 
   
